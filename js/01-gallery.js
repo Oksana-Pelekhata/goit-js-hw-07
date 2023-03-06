27476bc3a6d7f8c40203c80b164e3ffa92b739d0
Oksana-Pelekhata <ok.soloviova@gmail.com>
2023-03-06 14:07:12 +0200

    hw-7-1-with closing on Esc btn

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -26,6 +26,7 @@ galleryContainer.addEventListener('click', handleImgClick)
 function handleImgClick(event) {
     event.preventDefault();
 
+
     if (event.target.nodeName !== 'IMG') {
         return;
     }
@@ -34,6 +35,19 @@ function handleImgClick(event) {
     const instance = basicLightbox.create(
         `<img src="${largeImageUrl}" width="800" height="600">`
     )
-    
-    instance.show()
+    instance.show(() => {
+    window.addEventListener('keydown', onEscKeyPress)
+    })
+
+    function onEscKeyPress(event) {
+        const ESC_KEY_CODE = 'Escape';
+        const isEscKey = event.code === ESC_KEY_CODE;
+
+        if (isEscKey) {
+            instance.close(() => {
+            window.removeEventListener('keydown', onEscKeyPress)
+            })
+        }
+    }
+
 }
